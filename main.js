fe3cdbc4f2641e861f27dd8f9e3190563281cd8d
hyejingit <87255717+hyejingit@users.noreply.github.com>
2023-03-12 16:25:58 +0900

    html/css/js/img/font 파일추가
    작업중

---
+++ main.js
@@ -0,0 +1,37 @@
+//슬라이드
+let curPos = 0;
+let postion = 0;
+let start_x, end_x;
+const IMAGE_WIDTH = 196;
+const swipe = document.querySelector(".swipe");
+
+swipe.addEventListener("touchstart", touch_start);
+swipe.addEventListener("touchend", touch_end);
+
+function prev() {
+  if (curPos > 0) {
+    postion += IMAGE_WIDTH;
+    swipe.style.transform = `translateX(${postion}px)`;
+    curPos = curPos - 1;
+  }
+}
+function next() {
+  if (curPos < 16) {
+    postion -= IMAGE_WIDTH;
+    swipe.style.transform = `translateX(${postion}px)`;
+    curPos = curPos + 1;
+  }
+}
+
+function touch_start(event) {
+  start_x = event.touches[0].pageX;
+}
+
+function touch_end(event) {
+  end_x = event.changedTouches[0].pageX;
+  if (start_x > end_x) {
+    next();
+  } else {
+    prev();
+  }
+}
